e9c75622b09118ceb44f0698e8c0faf3acb9d3c4
Jesse Martinez <jessemartinez452@gmail.com>
2021-05-25 16:35:26 -0500

    Added Exception examples

--- src/main/java/calculatorExercise/Calculator.java
+++ src/main/java/calculatorExercise/Calculator.java
@@ -1,50 +1,50 @@
-package calculatorExercise;
-
-import java.util.Scanner;
-
-public class Calculator {
-
-	public static void main(String[] args) {
-		
-		int num1, num2;
-		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("CALCULATOR -- ENTER TWO NUMBERS\n");
-		
-		System.out.println("Enter first number: ");	
-		num1 = scanner.nextInt();
-		
-		System.out.println("Enter second number: ");
-		num2 = scanner.nextInt();
-		
-		System.out.println("Enter a number matching the operation to perform: \n 1. Add\n 2. Multiply\n 3. Division\n 4. Subtraction");
-		int result = scanner.nextInt();
-		
-		scanner.close();
-		
-		
-		switch(result) {
-		case 1:
-			result = num1 + num2;
-			break;
-		case 2: 
-			result = num1 * num2;
-			break;
-		case 3:
-			result = num1 / num2;
-			break;
-		case 4:
-			result = num1 - num2;
-			default:
-				System.out.println("You input was incorrect! ");
-				return;
-		}
-		System.out.println("Your result of your two numbers is : " + result);
-		
-		
-			
-		
-
-	}
-
-}
+//package calculatorExercise;
+//
+//import java.util.Scanner;
+//
+//public class Calculator {
+//
+//	public static void main(String[] args) {
+//		
+//		int num1, num2;
+//		
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("CALCULATOR -- ENTER TWO NUMBERS\n");
+//		
+//		System.out.println("Enter first number: ");	
+//		num1 = scanner.nextInt();
+//		
+//		System.out.println("Enter second number: ");
+//		num2 = scanner.nextInt();
+//		
+//		System.out.println("Enter a number matching the operation to perform: \n 1. Add\n 2. Multiply\n 3. Division\n 4. Subtraction");
+//		int result = scanner.nextInt();
+//		
+//		scanner.close();
+//		
+//		
+//		switch(result) {
+//		case 1:
+//			result = num1 + num2;
+//			break;
+//		case 2: 
+//			result = num1 * num2;
+//			break;
+//		case 3:
+//			result = num1 / num2;
+//			break;
+//		case 4:
+//			result = num1 - num2;
+//			default:
+//				System.out.println("You input was incorrect! ");
+//				return;
+//		}
+//		System.out.println("Your result of your two numbers is : " + result);
+//		
+//		
+//			
+//		
+//
+//	}
+//
+//}
--- src/main/java/com/qa/main/Runner.java
+++ src/main/java/com/qa/main/Runner.java
@@ -43,57 +43,115 @@
 //		scanner.close();
 //	}
 //}
+
+
+ //CALCUALTOR CODE ================================================================
+//package com.qa.main;
+//
+//import java.util.Scanner;
+//
+//public class Runner {
+//
+//	public static void main(String[] args) {
+//		
+//		//Declared the two numbers we need 
+//		int num1, num2;
+//		
+//		//Opened a new scanner input
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("CALCULATOR -- ENTER TWO NUMBERS\n");
+//		
+//		//Grabbing first number
+//		System.out.println("Enter first number: ");	
+//		num1 = scanner.nextInt();
+//		//Grabbing Second Number
+//		System.out.println("Enter second number: ");
+//		num2 = scanner.nextInt();
+//		
+//		//Grabbing the operation using number options as input
+//		System.out.println("Enter a number matching the operation to perform: \n 1. Add\n 2. Multiply\n 3. Division\n 4. Subtraction");
+//		int result = scanner.nextInt();
+//		
+//		scanner.close();
+//		
+//		// Using switch cases to choose right operation to perform
+//		switch(result) {
+//		case 1:
+//			result = num1 + num2;
+//			break;
+//		case 2: 
+//			result = num1 * num2;
+//			break;
+//		case 3:
+//			result = num1 / num2;
+//			break;
+//		case 4:
+//			result = num1 - num2;
+//			default:
+//				System.out.println("You input was incorrect! ");
+//				return;
+//		}
+//		System.out.println("Your result of your two numbers is : " + result);
+//		
+//
+//
+//	}
+//
+//}
+//=================================================================================
+
+
 package com.qa.main;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Runner {
 
-	public static void main(String[] args) {
-		
-		//Declared the two numbers we need 
-		int num1, num2;
-		
-		//Opened a new scanner input
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("CALCULATOR -- ENTER TWO NUMBERS\n");
-		
-		//Grabbing first number
-		System.out.println("Enter first number: ");	
-		num1 = scanner.nextInt();
-		//Grabbing Second Number
-		System.out.println("Enter second number: ");
-		num2 = scanner.nextInt();
-		
-		//Grabbing the operation using number options as input
-		System.out.println("Enter a number matching the operation to perform: \n 1. Add\n 2. Multiply\n 3. Division\n 4. Subtraction");
-		int result = scanner.nextInt();
-		
-		scanner.close();
-		
-		// Using switch cases to choose right operation to perform
-		switch(result) {
-		case 1:
-			result = num1 + num2;
-			break;
-		case 2: 
-			result = num1 * num2;
-			break;
-		case 3:
-			result = num1 / num2;
-			break;
-		case 4:
-			result = num1 - num2;
-			default:
-				System.out.println("You input was incorrect! ");
-				return;
-		}
-		System.out.println("Your result of your two numbers is : " + result);
-		
-		
-			
-		
+	public static Scanner scanner = new Scanner(System.in);
+	public static List<String> ourList;
 
+	public static void main(String[] args) {
+		try {
+			start();
+		} finally {
+			closeScanner();
+		}
+		
+	}
+	
+	public static void start() {
+		//This will print out the problem and continue to run
+		try {
+//			throw new RuntimeException("This fails because we say so");
+//			runException();
+			divCalculator(0 ,1);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			
+		}
+		System.out.println("Still running");
+	}
+	
+	public static void runException() throws IOException {
+		if(true) {
+			throw new IOException("You called this method wrong");
+		}
+	}
+	
+	public static void divCalculator(int x, int y)  {
+		int result = x / y;
+		if (x == 0) {
+			throw new ArithmeticException("Cannot divide by zero");
+		}
+		
+	}
+
+	
+	public static void closeScanner() {
+		scanner.close();
 	}
 
 }
